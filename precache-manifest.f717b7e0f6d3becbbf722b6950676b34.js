002bca7a7501df2f63a3d2a8f22a031381eb3887
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-09-08 11:23:41 +0200

    initial project

---
+++ precache-manifest.f717b7e0f6d3becbbf722b6950676b34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb4caa83454d49b354c3adb668a3536",
+    "url": "/wood-stock-coffee-shop/index.html"
+  },
+  {
+    "revision": "0da7ee91c4309654c4d8",
+    "url": "/wood-stock-coffee-shop/static/css/2.e76a8b4c.chunk.css"
+  },
+  {
+    "revision": "7def8030b3e822e50efe",
+    "url": "/wood-stock-coffee-shop/static/css/main.c4977666.chunk.css"
+  },
+  {
+    "revision": "0da7ee91c4309654c4d8",
+    "url": "/wood-stock-coffee-shop/static/js/2.49693b52.chunk.js"
+  },
+  {
+    "revision": "7def8030b3e822e50efe",
+    "url": "/wood-stock-coffee-shop/static/js/main.9e10699c.chunk.js"
+  },
+  {
+    "revision": "1650eac54041663f715d",
+    "url": "/wood-stock-coffee-shop/static/js/runtime~main.49030bae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wood-stock-coffee-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
